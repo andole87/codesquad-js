06396350569bc9072c3c7024ed41df860ce0e505
andole87 <andole87@gmail.com>
2019-04-02 17:29:36 +0900

    구름 ide 정보 추가 노드버전, 플젝디렉토리, ssh 정보 주석으로 추가

--- 1-3/hello.js
+++ 1-3/hello.js
@@ -4,3 +4,8 @@ var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server is listening on 8080...');
 });
+
+/*구름 IDE 환경*/
+//node 10
+//프로젝트루트 ~/workspace/step1/1-3/server.js
+//ssh -p 52725 root@13.125.109.66
